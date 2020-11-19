1c35b4e068072fcd1e6fe095a42021d43384a950
Snep <61911701+SnepGaming@users.noreply.github.com>
2020-11-19 20:29:51 +0100

    Lavoro del 19/11

--- main.cpp
+++ main.cpp
@@ -2,13 +2,34 @@
 #include "giocatore.h"
 #include "computer.h"
 #include "gestione.h"
-//#include "ghrandom.h"
-#include "random.hpp"
+#include "librerie/random.hpp"
 #include <stdlib.h>
 #include <windows.h> 
 
 using namespace std;
 
 int main ()
+{
+    HANDLE hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, 10);
+    
+int m_eta, m_genere, n;
+string nc;
+string c;
+getline(cin,c);
+lv_giocatore cv;
+cout<<"n"<<endl;
+cin>>n;
+cv.load(n, nc, m_eta, m_genere);
+cv.visualizza_eta(n);
+int sel;//da implementare in gestione.h
+cout<<"sel"<<endl;
+cin>>sel;
+//controllo cn;
+simulazione sn;
+//cn.selezione(sel,1);
+sn.simula_maschio(1);
+cv.visualizza_vivo_morto(n);
     return 0; //merge
 }
